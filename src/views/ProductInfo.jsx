c151586e9daf548223cdc6c5811dbf773116e23f
Daniel Rosa <danielrosaa@gmail.com>
2021-01-30 22:05:25 -0300

    Fix useEffect error.

--- src/views/ProductInfo.jsx
+++ src/views/ProductInfo.jsx
@@ -1,39 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { InnerWrapper } from './../style/mainStyle'
-import { Product } from './../services/Sneakers'
-import Breadcrumbs from '../components/ProductInfo/Breadcrumbs'
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { InnerWrapper } from "./../style/mainStyle"
+import { Product } from "./../services/Sneakers"
+import Breadcrumbs from "../components/ProductInfo/Breadcrumbs"
 import PaymentCardInfo from "../components/ProductInfo/PaymentCardInfo"
+import Receipt from "../components/Receipt/Receipt"
 
 const PaymentInfo = styled.div`
-  width: 100%;
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1.5fr 1.8fr;
-  grid-gap: 3%;
+	width: 100%;
+	height: 100%;
+	display: grid;
+	grid-template-columns: 1.5fr 1.8fr;
+	grid-gap: 3%;
 `
 const ProductImage = styled.div`
-  background-image: url("${props => props.image}");
-  background-size: cover;
-  background-position: center;
-  border-radius: 8px;
-  `
+	background-image: url("${props => props.image}");
+	background-size: cover;
+	background-position: center;
+	border-radius: 8px;
+`
 
 const ProductInfo = ({ match }) => {
   const [productInfo, setProducInfo] = useState({})
+  const [renderReceipt, setRenderReceipt] = useState(false)
 
-  useEffect(() => {
-    Product(match.params.id).then(info => setProducInfo(info))
-  })
+	useEffect(() => {
+		Product(match.params.id).then(info => setProducInfo(info))
+		window.PayWithMyBank.addPanelListener(function (command, event) {
+			if (command === "event" && event.type === "new_location") {
+				if (event.data.indexOf("#success") === 0) {
+          setRenderReceipt(true)
+          console.log(event.data)
+				} else {
+          setRenderReceipt(false)
+          console.log(event.data)
+				}
+				return false
+			}
+    })
+  }, [match.params.id])
 
-  return (
+	return (
 		<InnerWrapper>
-			<Breadcrumbs />
+			<Breadcrumbs status={renderReceipt} />
 			<PaymentInfo>
 				<ProductImage image={productInfo.maxresURL}></ProductImage>
 				<div>
-          <PaymentCardInfo product={productInfo} />
-        </div>
+					{!renderReceipt ? (
+						<PaymentCardInfo product={productInfo} />
+					) : (
+						<Receipt product={productInfo} />
+					)}
+				</div>
 			</PaymentInfo>
 		</InnerWrapper>
 	)
